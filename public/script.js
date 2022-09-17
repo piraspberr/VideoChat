a6b8dadb0a7b6f4aa28f5180427423484e9a458f
Ylyas <86777995+piraspberr@users.noreply.github.com>
2022-09-17 15:01:11 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io("../views/socket.io.js");
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 const showChat = document.querySelector("#showChat");
